6371d76ae6364926c1f16b157e18243bab6f68b2
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-01-17 02:21:12 +0900

    タスクのステータスでの分類機能を実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :task_name, :task_content, :status, :priority, :deadline, presence: true
   validates :task_name, :task_content, length:{ minimum: 1 }
+  enum status:{ '未着手':0, '着手中':1, '完了':2 }
 end
