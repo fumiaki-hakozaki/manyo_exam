401a96feb16cce0405eca51b1cc027955b024af1
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-01-18 11:28:21 +0900

    検索インデックス作成、デバッグインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_154927) do
+ActiveRecord::Schema.define(version: 2023_01_18_022352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2023_01_17_154927) do
     t.integer "priority", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_name", "status"], name: "index_tasks_on_task_name_and_status"
   end
 
 end
